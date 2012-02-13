3339efde0bf9069b6a8409138ba89e495e58dbf9
Elvin (Home Windows7) <elvin.luo@gmail.com>
2012-02-13 22:07:46 +0800

    Some code issue. Signed-off-by: Elvin (Home Windows7) <elvin.luo@gmail.com>

--- src/net/micode/fileexplorer/FileExplorerTabActivity.java
+++ src/net/micode/fileexplorer/FileExplorerTabActivity.java
@@ -18,6 +18,7 @@
  */
 
 package net.micode.fileexplorer;
+
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.Activity;
@@ -32,159 +33,160 @@ import android.view.ActionMode;
 import java.util.ArrayList;
 
 public class FileExplorerTabActivity extends Activity {
-    private static final String INSTANCESTATE_TAB = "tab";
-    ViewPager mViewPager;
-    TabsAdapter mTabsAdapter;
-    ActionMode mActionMode;
+	private static final String INSTANCESTATE_TAB = "tab";
+	ViewPager mViewPager;
+	TabsAdapter mTabsAdapter;
+	ActionMode mActionMode;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.fragment_pager);
-        mViewPager = (ViewPager) findViewById(R.id.pager);
+		setContentView(R.layout.fragment_pager);
+		mViewPager = (ViewPager) findViewById(R.id.pager);
 
-        final ActionBar bar = getActionBar();
-        bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE | ActionBar.DISPLAY_SHOW_HOME);
+		final ActionBar bar = getActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		bar.setDisplayOptions(0, ActionBar.DISPLAY_SHOW_TITLE
+				| ActionBar.DISPLAY_SHOW_HOME);
 
-        mTabsAdapter = new TabsAdapter(this, mViewPager);
-        mTabsAdapter.addTab(bar.newTab().setText(R.string.tab_sd),
-                FileViewActivity.class, null);
-        mTabsAdapter.addTab(bar.newTab().setText(R.string.tab_remote),
-                ServerControlActivity.class, null);
-        if (savedInstanceState != null) {
-            bar.setSelectedNavigationItem(savedInstanceState.getInt(INSTANCESTATE_TAB, 0));
-        }
-    }
+		mTabsAdapter = new TabsAdapter(this, mViewPager);
+		mTabsAdapter.addTab(bar.newTab().setText(R.string.tab_sd),
+				FileViewActivity.class, null);
+		mTabsAdapter.addTab(bar.newTab().setText(R.string.tab_remote),
+				ServerControlActivity.class, null);
+		if (savedInstanceState != null) {
+			bar.setSelectedNavigationItem(savedInstanceState.getInt(
+					INSTANCESTATE_TAB, 0));
+		}
+	}
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putInt(INSTANCESTATE_TAB, getActionBar().getSelectedNavigationIndex());
-    }
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt(INSTANCESTATE_TAB, getActionBar()
+				.getSelectedNavigationIndex());
+	}
 
-    @Override
-    public void onBackPressed() {
-        IBackPressedListener backPressedListener = (IBackPressedListener) mTabsAdapter
-                .getItem(mViewPager.getCurrentItem());
-        if (!backPressedListener.onBack()) {
-            super.onBackPressed();
-        }
-    }
+	@Override
+	public void onBackPressed() {
+		IBackPressedListener backPressedListener = (IBackPressedListener) mTabsAdapter
+				.getItem(mViewPager.getCurrentItem());
+		if (!backPressedListener.onBack()) {
+			super.onBackPressed();
+		}
+	}
 
-    public interface IBackPressedListener {
-        /**
-         * 澶勭悊back浜嬩欢銆�
-         * @return True: 琛ㄧず宸茬粡澶勭悊; False: 娌℃湁澶勭悊锛岃鍩虹被澶勭悊銆�
-         */
-        boolean onBack();
-    }
+	public interface IBackPressedListener {
+		/**
+		 * 处理back事件。
+		 * 
+		 * @return True: 表示已经处理; False: 没有处理，让基类处理。
+		 */
+		boolean onBack();
+	}
 
-    public void setActionMode(ActionMode actionMode) {
-        mActionMode = actionMode;
-    }
+	public void setActionMode(ActionMode actionMode) {
+		mActionMode = actionMode;
+	}
 
-    public ActionMode getActionMode() {
-        return mActionMode;
-    }
+	public ActionMode getActionMode() {
+		return mActionMode;
+	}
 
-    /**
-     * This is a helper class that implements the management of tabs and all
-     * details of connecting a ViewPager with associated TabHost.  It relies on a
-     * trick.  Normally a tab host has a simple API for supplying a View or
-     * Intent that each tab will show.  This is not sufficient for switching
-     * between pages.  So instead we make the content part of the tab host
-     * 0dp high (it is not shown) and the TabsAdapter supplies its own dummy
-     * view to show as the tab content.  It listens to changes in tabs, and takes
-     * care of switch to the correct paged in the ViewPager whenever the selected
-     * tab changes.
-     */
-    public static class TabsAdapter extends FragmentPagerAdapter
-            implements ActionBar.TabListener, ViewPager.OnPageChangeListener {
-        private final Context mContext;
-        private final ActionBar mActionBar;
-        private final ViewPager mViewPager;
-        private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+	/**
+	 * This is a helper class that implements the management of tabs and all
+	 * details of connecting a ViewPager with associated TabHost. It relies on a
+	 * trick. Normally a tab host has a simple API for supplying a View or
+	 * Intent that each tab will show. This is not sufficient for switching
+	 * between pages. So instead we make the content part of the tab host 0dp
+	 * high (it is not shown) and the TabsAdapter supplies its own dummy view to
+	 * show as the tab content. It listens to changes in tabs, and takes care of
+	 * switch to the correct paged in the ViewPager whenever the selected tab
+	 * changes.
+	 */
+	public static class TabsAdapter extends FragmentPagerAdapter implements
+			ActionBar.TabListener, ViewPager.OnPageChangeListener {
+		private final Context mContext;
+		private final ActionBar mActionBar;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
 
-        static final class TabInfo {
-            private final Class<?> clss;
-            private final Bundle args;
-            private Fragment fragment;
+		static final class TabInfo {
+			private final Class<?> clss;
+			private final Bundle args;
+			private Fragment fragment;
 
-            TabInfo(Class<?> _class, Bundle _args) {
-                clss = _class;
-                args = _args;
-            }
-        }
+			TabInfo(Class<?> _class, Bundle _args) {
+				clss = _class;
+				args = _args;
+			}
+		}
 
-        public TabsAdapter(Activity activity, ViewPager pager) {
-            super(activity.getFragmentManager());
-            mContext = activity;
-            mActionBar = activity.getActionBar();
-            mViewPager = pager;
-            mViewPager.setAdapter(this);
-            mViewPager.setOnPageChangeListener(this);
-        }
+		public TabsAdapter(Activity activity, ViewPager pager) {
+			super(activity.getFragmentManager());
+			mContext = activity;
+			mActionBar = activity.getActionBar();
+			mViewPager = pager;
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
 
-        public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-            TabInfo info = new TabInfo(clss, args);
-            tab.setTag(info);
-            tab.setTabListener(this);
-            mTabs.add(info);
-            mActionBar.addTab(tab);
-            notifyDataSetChanged();
-        }
+		public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
+			TabInfo info = new TabInfo(clss, args);
+			tab.setTag(info);
+			tab.setTabListener(this);
+			mTabs.add(info);
+			mActionBar.addTab(tab);
+			notifyDataSetChanged();
+		}
 
-        @Override
-        public int getCount() {
-            return mTabs.size();
-        }
+		@Override
+		public int getCount() {
+			return mTabs.size();
+		}
 
-        @Override
-        public Fragment getItem(int position) {
-            TabInfo info = mTabs.get(position);
-            if (info.fragment == null) {
-                info.fragment = Fragment.instantiate(mContext, info.clss.getName(), info.args);
-            }
-            return info.fragment;
-        }
+		@Override
+		public Fragment getItem(int position) {
+			TabInfo info = mTabs.get(position);
+			if (info.fragment == null) {
+				info.fragment = Fragment.instantiate(mContext,
+						info.clss.getName(), info.args);
+			}
+			return info.fragment;
+		}
 
-        
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        }
+		public void onPageScrolled(int position, float positionOffset,
+				int positionOffsetPixels) {
+		}
 
-        
-        public void onPageSelected(int position) {
-            mActionBar.setSelectedNavigationItem(position);
-        }
+		public void onPageSelected(int position) {
+			mActionBar.setSelectedNavigationItem(position);
+		}
 
-        
-        public void onPageScrollStateChanged(int state) {
-        }
+		public void onPageScrollStateChanged(int state) {
+		}
 
-        
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            Object tag = tab.getTag();
-            for (int i=0; i<mTabs.size(); i++) {
-                if (mTabs.get(i) == tag) {
-                    mViewPager.setCurrentItem(i);
-                }
-            }
-            if(!tab.getText().equals(mContext.getString(R.string.tab_sd))) {
-                ActionMode actionMode = ((FileExplorerTabActivity) mContext).getActionMode();
-                if (actionMode != null) {
-                    actionMode.finish();
-                }
-            }
-        }
+		public void onTabSelected(Tab tab, FragmentTransaction ft) {
+			Object tag = tab.getTag();
+			for (int i = 0; i < mTabs.size(); i++) {
+				if (mTabs.get(i) == tag) {
+					mViewPager.setCurrentItem(i);
+				}
+			}
+			if (!tab.getText().equals(mContext.getString(R.string.tab_sd))) {
+				ActionMode actionMode = ((FileExplorerTabActivity) mContext)
+						.getActionMode();
+				if (actionMode != null) {
+					actionMode.finish();
+				}
+			}
+		}
 
-        
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-        }
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		}
 
-        
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {
-        }
-    }
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		}
+	}
 }
